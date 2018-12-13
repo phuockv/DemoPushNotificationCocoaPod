4fbbd08e49d92f7fc74e9662f95b86014f0e1da4
Phuoc <phuoc.kieu@sutrixsolutions.com>
2018-12-13 09:39:13 +0700

    update version 0.1.1

--- DemoPushNotificationCocoaPod.podspec
+++ DemoPushNotificationCocoaPod.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'DemoPushNotificationCocoaPod'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'A short description of DemoPushNotificationCocoaPod.'
 
 # This description is used to generate tags and improve search results.
@@ -17,10 +17,7 @@ Pod::Spec.new do |s|
 #   * Write the description between the DESC delimiters below.
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
-  s.description      = <<-DESC
-TODO: Add long description of the pod here.
-                       DESC
-
+  s.description      = 'Use amazon service to push notificaiton and interact with native scrips code'
   s.homepage         = 'https://github.com/phuockv/DemoPushNotificationCocoaPod'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
@@ -28,7 +25,7 @@ TODO: Add long description of the pod here.
   s.source           = { :git => 'https://github.com/phuockv/DemoPushNotificationCocoaPod.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '9.0'
 
 s.dependency 'AWSCore', '~> 2.7.0'
 s.dependency 'AWSPinpoint', '~> 2.7.0'
@@ -36,8 +33,9 @@ s.dependency 'AWSAuthCore', '~> 2.7.0'
 s.dependency 'AWSMobileClient', '~> 2.7.0'
 s.dependency 'AWSCognitoIdentityProvider', '~> 2.7.0'
 s.dependency 'AWSCognitoIdentityProviderASF','~> 1.0.1'
-s.platform     = :ios, '9.2'
-s.swift_version = '4.2'
+s.source_files = 'DemoPushNotificationCocoaPod/Classes/**/*.swift'
+s.platform     = :ios, '9.0'
+s.swift_version = '4.1'
 
   # s.resource_bundles = {
   #   'DemoPushNotificationCocoaPod' => ['DemoPushNotificationCocoaPod/Assets/*.png']
---
+++ DemoPushNotificationCocoaPod/Classes/NotificationPlugin.swift
@@ -0,0 +1,103 @@
+//
+//  hello.swift
+//  Pods
+//
+//  Created by dev on 12/10/18.
+//
+
+//
+//  pluginPushNotification.swift
+//  pluginPushNotification
+//
+//  Created by dev on 11/29/18.
+//  Copyright © 2018 pluginPushNotification. All rights reserved.
+//
+
+import Foundation
+import AWSCore
+
+import AWSPinpoint
+import AWSMobileClient
+import UserNotifications
+
+@objc public class NotificationPlugin: NSObject ,UNUserNotificationCenterDelegate{
+    var pinpoint: AWSPinpoint?
+    @objc public func registerForPushNotifications(didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) {
+        AWSMobileClient.sharedInstance().initialize { (userState, error) in
+            if let error = error {
+                print("Error initializing AWSMobileClient: \(error.localizedDescription)")
+            } else if let userState = userState {
+                print("AWSMobileClient initialized. Current UserState: \(userState.rawValue)")
+            }
+        }
+        
+        // Initialize Pinpoint
+        let pinpointConfiguration = AWSPinpointConfiguration.defaultPinpointConfiguration(launchOptions: launchOptions)
+        pinpoint = AWSPinpoint(configuration: pinpointConfiguration)
+        
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self
+        } else {
+            // Fallback on earlier versions
+        }
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+                (granted, error) in
+                print("Permission granted: \(granted)")
+                // 1. Check if permission granted
+                guard granted else { return }
+                // 2. Attempt registration for remote notifications on the main thread
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    @objc public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        pinpoint!.notificationManager.interceptDidRegisterForRemoteNotifications(
+            withDeviceToken: deviceToken)
+        let tokenParts = deviceToken.map { data -> String in
+            return String(format: "%02.2hhx", data)
+        }
+        let token = tokenParts.joined()
+        // 2. Print device token to use for PNs payloads
+        print("Device Token: \(token)")
+    }
+    
+    @objc public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if let aps = userInfo["aps"] as? NSDictionary {
+            if let alert = aps["alert"] as? NSDictionary {
+                let body = alert["body"] as? String
+                let title = alert["title"] as? String
+                pinpoint!.notificationManager.interceptDidReceiveRemoteNotification(
+                    userInfo, fetchCompletionHandler: completionHandler)
+                
+                if (application.applicationState == .active) {
+                    let alert = UIAlertController(title: title,
+                                                  message: body,
+                                                  preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                    
+                    UIApplication.shared.keyWindow?.rootViewController?.present(
+                        alert, animated: true, completion:nil)
+                }
+            }
+        }
+        
+    }
+    
+    @objc public func applicationDidEnterBackground(_ application: UIApplication) {
+        if let targetingClient = pinpoint?.targetingClient {
+            targetingClient.addAttribute(["science", "politics", "travel"], forKey: "interests")
+            targetingClient.updateEndpointProfile()
+            let endpointId = targetingClient.currentEndpointProfile().endpointId
+            print("Updated custom attributes for endpoint: \(endpointId)")
+        }
+        
+    }
+    
+}
+
