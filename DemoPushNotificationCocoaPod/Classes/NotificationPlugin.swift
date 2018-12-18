0526abbc201f89b7bbeb4f11f62073e58baa67a3
Phuoc <phuoc.kieu@sutrixsolutions.com>
2018-12-18 15:42:41 +0700

    update 0.1.11

--- DemoPushNotificationCocoaPod/Classes/NotificationPlugin.swift
+++ DemoPushNotificationCocoaPod/Classes/NotificationPlugin.swift
@@ -20,11 +20,9 @@ import AWSPinpoint
 import AWSMobileClient
 import UserNotifications
 
-@UIApplicationMain
-@objc public class NotificationPlugin: NSObject ,UNUserNotificationCenterDelegate, UIApplicationDelegate {
-    var pinpoint: AWSPinpoint?
-    
-    @objc public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+@objc public class NotificationPlugin: NSObject, UNUserNotificationCenterDelegate, UIApplicationDelegate {
+    @objc open var pinpoint: AWSPinpoint?
+    @objc public func registerForPushNotifications(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) {
         AWSMobileClient.sharedInstance().initialize { (userState, error) in
             if let error = error {
                 print("Error initializing AWSMobileClient: \(error.localizedDescription)")
@@ -41,28 +39,24 @@ import UserNotifications
             UNUserNotificationCenter.current().delegate = self
         } else {
             // Fallback on earlier versions
+            print("Fallback on earlier versions")
         }
         if #available(iOS 10.0, *) {
             UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
                 (granted, error) in
                 print("Permission granted: \(granted)")
-                
                 // 1. Check if permission granted
-                guard granted else {
-                    return
-                    
-                }
-                
+                guard granted else { return }
                 // 2. Attempt registration for remote notifications on the main thread
                 DispatchQueue.main.async {
                     UIApplication.shared.registerForRemoteNotifications()
-                    
                 }
             }
         } else {
             // Fallback on earlier versions
+            print("Fallback on earlier versions")
+
         }
-        return true
     }
     
     @objc public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
@@ -75,11 +69,6 @@ import UserNotifications
         // 2. Print device token to use for PNs payloads
         print("Device Token: \(token)")
     }
-    public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print(error.localizedDescription)
-    }
-    
-    
     
     @objc public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         if let aps = userInfo["aps"] as? NSDictionary {
