b4dad782b7314dda77462d13d0bbde85247f6fbf
Phuoc <phuoc.kieu@sutrixsolutions.com>
2018-12-24 14:23:33 +0700

    update file NotificationPlugin

--- DemoPushNotificationCocoaPod/Classes/NotificationPlugin.swift
+++ DemoPushNotificationCocoaPod/Classes/NotificationPlugin.swift
@@ -25,7 +25,13 @@ import UserNotifications
 //        let dataToken = Data(deviceToken.utf8)
         pinpoint!.notificationManager.interceptDidRegisterForRemoteNotifications(
             withDeviceToken: deviceToken)
-   
+        
+        let tokenParts = deviceToken.map { data -> String in
+            return String(format: "%02.2hhx", data)
+        }
+        let token = tokenParts.joined()
+        // 2. Print device token to use for PNs payloads
+        print("Device Token: \(token)")
     }
 
 //    @objc public func didRegisterForRemoteNotificationsWithDeviceToken(_ deviceToken: String) {
